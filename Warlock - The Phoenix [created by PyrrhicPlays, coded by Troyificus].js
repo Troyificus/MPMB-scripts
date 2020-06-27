2da7beddbdd4ba7f69bb86f27a7f3387ae7983a7
Troyificus <39455501+Troyificus@users.noreply.github.com>
2020-06-27 09:59:11 +0100

    Update Warlock - The Phoenix [created by PyrrhicPlays, coded by Troyificus].js
    Changed perspective style ('My' to 'Your' etc).

--- Warlock - The Phoenix [created by PyrrhicPlays, coded by Troyificus].js
+++ Warlock - The Phoenix [created by PyrrhicPlays, coded by Troyificus].js
@@ -28,27 +28,27 @@ AddSubClass("warlock", "the phoenix", {
 			name : "The Phoenix's Flames",
 			source : ["D&DB", 0],
 			minlevel : 1,
-			description : "\n   " + "My pact with the Phoenix gives me the ability to speak, read and write Ignan",
+			description : "\n   " + "Your pact with the Phoenix gives you the ability to speak, read and write Ignan",
 		},
 		"subclassfeature6" : {
 			name : "Ashen Protection",
 			source : ["D&DB", 0],
 			minlevel : 6,
-			description : "\n   " + "My skin and armor gains a light ashen appearance that never really seems to go away despite all cleaning. The ash protects me from fire, giving me resistance to fire damage.",
+			description : "\n   " + "Your skin and armor gains a light ashen appearance that never really seems to go away despite all cleaning. The ash protects you from fire, giving you resistance to fire damage.",
 			dmgres : ["Fire"]
 		},
 		"subclassfeature6.1" : {
 			name : "Ashen Step",
 			source : ["D&DB", 0],
 			minlevel : 6,
-			description : "\n   " + "Once per short rest, whenever I take damage, I may use my Reaction to halve the damage against me and disappear into a cloud of ash teleporting up to 30 feet to an unoccupied space I can see.",
+			description : "\n   " + "Once per short rest, whenever you take damage, you may use your Reaction to halve the damage against you and disappear into a cloud of ash, teleporting up to 30 feet to an unoccupied space you can see.",
 			action : ["reaction", ""]
 		},
 		"subclassfeature10" : {
 			name : "Flame Within",
 			source : ["D&DB", 0],
 			minlevel : 10,
-			description : "\n   " + "As an action my body becomes flame and blazing wings sprout from my back for 1 minute. For the duration I gain these benefits:" + "\n   " + "-I am immune to fire damage and resistant to non-magical weapons." + "\n   " + "-Any creature that touches or hits me with a melee attack while within 5 feet of me takes 1d10 fire damage." + "\n   " + "-My weapons and spells are wrapped in flames, and I add 1d6 fire damage to all damage rolls." + "\n   " + "-I have a fly speed of 30 feet." + "\n   " + "-I shed bright light for 30 feet and dim light for another 30 feet." + "\n   " + "-I can use Flame Within a number of times per day equal to my CON modifier (minmum of 1). Each use results in a point of exhaustion once the duration has completed as I am left drained." + "\n   " + "-Should the use of Flame Within result in my sixth exhaustion point, I die. If I have access to it and it has not been used in the past 72 hours, Rebirth is automatically triggered by death in this way.",
+			description : "\n   " + "As an action your body becomes flame and blazing wings sprout from your back for 1 minute. For the duration you gain the following benefits:" + "\n   " + "-You are immune to fire damage and resistant to non-magical weapons." + "\n   " + "-Any creature that touches or hits you with a melee attack while within 5 feet of you takes 1d10 fire damage." + "\n   " + "-Your weapons and spells are wrapped in flames, and you add 1d6 fire damage to all damage rolls." + "\n   " + "-You have a fly speed of 30 feet." + "\n   " + "-You shed bright light for 30 feet and dim light for another 30 feet." + "\n   " + "-You can use Flame Within a number of times per day equal to your CON modifier (minmum of 1). Each use results in a point of exhaustion once the duration has completed as you are left drained." + "\n   " + "-Should the use of Flame Within result in your sixth exhaustion point, you die. If you have access to it and it has not been used in the past 72 hours, Rebirth is automatically triggered by death in this way.",
 			action : ["action", ""]
 		},
 		"subclassfeature14" : {
